6aa383bc5f46a817d66791525825ad3b838519b5
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-03-04 12:58:26 +0200

    First commit

---
+++ precache-manifest.520d65cbaec4139761cb4aa9a0dab305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d85504762441f79342412673f124311",
+    "url": "/catalizator/index.html"
+  },
+  {
+    "revision": "1fe2aa75ef70e6810040",
+    "url": "/catalizator/static/css/main.d7759d16.chunk.css"
+  },
+  {
+    "revision": "e0c73b4bb453e06df022",
+    "url": "/catalizator/static/js/2.c80643d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/catalizator/static/js/2.c80643d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe2aa75ef70e6810040",
+    "url": "/catalizator/static/js/main.df8474d8.chunk.js"
+  },
+  {
+    "revision": "740f9384478b4cf8449c",
+    "url": "/catalizator/static/js/runtime-main.06893fdb.js"
+  }
+]);
