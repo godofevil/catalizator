254e2a2652428b2de104ffbeb9d50084d45a9155
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-03-04 12:53:32 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Loader from './components/Loader'
+import './App.sass'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: '',
+            imageLoaded: false
+        }
+    }
+
+    getImage = () => {
+        this.setState({imageLoaded: false});
+        fetch('https://api.thecatapi.com/v1/images/search')
+          .then(response => response.json())
+          .then(response => this.setState({url: response[0].url}))
+          .catch(error => alert(error))
+     }
+
+    componentDidMount = () => this.getImage();
+
+    render() {
+        return (
+            <div className="app">
+                <p className="app__title">Catalizator</p>
+                <img
+                    onLoad={() => this.setState({imageLoaded: true})}
+                    className="app__image" src={this.state.url}
+                />
+                <button onClick={this.getImage} className="app__button">
+                    {!this.state.imageLoaded ? <Loader /> : 'Next'}
+                </button>                
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/Loader/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.css';
+
+const Loader = () =>
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+
+export default Loader;
